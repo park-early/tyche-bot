955870f3384ab39c30e4ae8c3b5cb6c3ce6f9647
Parker Lee <parkerlee247@gmail.com>
2022-08-01 19:03:03 -0700

    Clean up guild command scripts

---
+++ commandscripts/delete-commands.js
@@ -0,0 +1,14 @@
+/**
+ * Set the value of commandId to the id of the command you wish to delete before running this script.
+ */
+const { Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { clientId, guildId, token } = require('../config.json');
+const rest = new REST({ version: '10' }).setToken(token);
+
+const commandId = '1003833870174654570';
+
+// DELETE request sent to a defined URL with ids as path variables
+rest.delete(Routes.applicationGuildCommand(clientId, guildId, commandId))
+	.then(() => console.log('Sucessfully deleted guild command'))
+	.catch(console.error);
--- commandscripts/deploy-commands.js
+++ commandscripts/deploy-commands.js
@@ -1,7 +1,13 @@
+/**
+ * Provide this script the commands you wish tyche-bot to have.
+ * Be sure to update this file if changes are made to commands.
+ * If changes are made, this script must be re-run.
+ */
 const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, token } = require('../config.json');
 
+// define some commands and convert to JSON for the request body
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
@@ -10,6 +16,7 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token);
 
+// PUT request sent to a defined URL with the ids as path variables and the commands in the request body
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Succesfully registered application commands'))
 	.catch(console.error);
